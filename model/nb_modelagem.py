eb994b01277238d5370dd4f466cc141e345a24c5
emmanuelvrm <87373868+emmanuelvrm@users.noreply.github.com>
2022-11-20 19:11:36 -0300

    linha horizontal

--- model/nb_modelagem.py
+++ model/nb_modelagem.py
@@ -98,6 +98,7 @@ with st.form("formulario"):
         st.write('Generos:', *gens, sep=" ")
         st.write('Transmissões: ',df_selecao['Transmissões'][0])
         #st.write(df_selecao)
+        st.markdown("""---""")
         st.write('\n\n')
     except:
       st.write("Execute novamente")
